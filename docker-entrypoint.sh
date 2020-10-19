fadbb18a65ca2fdf6eff9c37ebbc35e7221c7e97
Antonio Carlos Furtado <marcojrfurtado@gmail.com>
2020-10-18 21:38:09 -0400

    Add Proxy Server to docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,5 +11,8 @@ fi
 
 /bin/cp -up /ocean-provider/artifacts/* /usr/local/artifacts/ 2>/dev/null || true
 
-gunicorn -b ${OCEAN_PROVIDER_URL#*://} -w ${OCEAN_PROVIDER_WORKERS} -t ${OCEAN_PROVIDER_TIMEOUT} ocean_provider.run:app
+gunicorn -b ${OCEAN_PROVIDER_URL#*://} -w ${OCEAN_PROVIDER_WORKERS} -t ${OCEAN_PROVIDER_TIMEOUT} ocean_provider.run:app &
+
+gunicorn -b ${PROXY_SERVER_HOST}:${PROXY_SERVER_PORT} -w ${PROXY_SERVER_WORKERS} -t ${PROXY_SERVER_TIMEOUT} osmosis_streaming_driver.proxy_server.run:app
+
 tail -f /dev/null
